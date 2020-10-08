c4ef584b0c60943d975ee758019928c3113a89ef
andreileonsalas <andreileonsalas@gmail.com>
2020-10-08 13:42:11 -0600

    arreglo de intliteral, agregado de debug info

--- Funciones.c
+++ Funciones.c
@@ -1,7 +1,7 @@
 
 #include "Funciones.h"
 
-RegTS TS[1000] = { {"begin", BEGIN}, {"end", END}, {"read", READ}, {"write", WRITE},{"$", 99} };
+RegTS TS[1000] = { {"$", 99} };
 int numeroVariableTemporal=1;
 
 /*----------------------------------------------------Validacion-----------------------------------*/
@@ -133,9 +133,14 @@ void primary(expr_rec * result){
 /* <addop> ::= PLUSOP | MINUSOP */
 void add_op(char * result){
 	token tok = next_token();
+	printf("el operador asignado es: %d\n", tok);
 	if (tok == PLUSOP || tok == MINUSOP) {
+		printf("el operador asignado es: %d\n", tok);
 		match(tok);
+		printf("el operador asignado es: %d\n", tok);
 		strcpy(result, process_op());
+		printf("el operador asignado es: %d\n", tok);
+		printf(result);
 	} else
 		syntax_error();
 }
@@ -178,6 +183,7 @@ void expr_list(void){
 token next_token(void){
 	if (flagToken!=1) {
 		current_token = scanner();
+		//printf("%d", current_token);
 		flagToken = 1;
 		if(current_token == LEXICALERROR) {
 			lexical_error();
@@ -236,201 +242,212 @@ token check_reserved(void)
 	}
 }
 
-// token scanner ()
-// {
-//
-// 	int in_char, c;
-// 	int i=0;
-// 	clear_buffer();//limpia el array
-// 	if (feof(archive))
-// 		return SCANEOF;
-//
-//
-// 	while ((in_char = getc(archive)) != EOF) {
-// 		//printf("%c\n",in_char);
-//
-// 		if (isspace(in_char))
-// 			continue; /* do nothing */
-// 		else if (isalpha(in_char)) {//revisa si el caracter es alfabetico
-//
-// 			buffer_char(in_char, i);//agrega caracter al buffer
-// 			i++;
-//
-// 			for (c = getc(archive); isalnum(c) || c == '_'; c = getc(archive)) { //recorre la hilera agregando caracteres al buffer_char, termina cuando no es un caracter ni underscore
-// 				buffer_char(c, i);
-// 				i++;
-// 			}
-// 			ungetc(c,archive);
-//
-// 			return check_reserved(); //Revisa si el string encontrado es una palabra reservada
-//
-// 		}  else if (isdigit(in_char)) {
-// 			buffer_char(in_char, i);
-// 			i++;
-// 			for (c = getc(archive); isdigit(c); c = getc(archive))
-// 				buffer_char(c, i);
-// 			i++;
-// 			ungetc(c,archive);
-//
-// 			//token_buffer[ncol] = '\0';
-// 			//printf( "Token_Buffer = %s\n", &token_buffer[0]);
-//
-// 			return INTLITERAL;
-//
-// 		} else if (in_char == '(')
-// 			return LPAREN;
-// 		else if (in_char == ')')
-// 			return RPAREN;
-// 		else if (in_char == ';')
-// 			return SEMICOLON;
-// 		else if (in_char == ',')
-// 			return COMMA;
-// 		else if (in_char == '+') {
-// 			return PLUSOP;
-// 		}
-// 		else if (in_char == ':') {
-// 			/* looking for ":=" */
-// 			c = getc(archive);
-// 			if (c == '=') {
-// 				return ASSIGNOP;
-// 			}
-// 			else {
-// 				ungetc(c,archive);
-// 				lexical_error();//
-// 			}
-// 		} else if (in_char == '-') {
-// 			/* is it --, comment start */
-// 			c = getc(archive);
-// 			if (c == '-') {
-// 				do
-// 				{
-// 					in_char = getc(archive);
-// 				} while (in_char != '\n');
-// 			} else {
-// 				ungetc(c,archive);
-// 				return MINUSOP;
-// 			}
-// 		}  else
-// 			lexical_error();
+token scanner ()
+{
+
+	int in_char, c;
+	int i=0;
+	clear_buffer();//limpia el array
+	if (feof(archive))
+		return SCANEOF;
+
+
+	while ((in_char = getc(archive)) != EOF) {
+		printf("caracter actual es:%c\n",in_char);
+
+		if (isspace(in_char))
+			continue; /* do nothing */
+		else if (isalpha(in_char)) {//revisa si el caracter es alfabetico
+
+			buffer_char(in_char, i);//agrega caracter al buffer
+			i++;
+
+			for (c = getc(archive); isalnum(c) || c == '_'; c = getc(archive)) { //recorre la hilera agregando caracteres al buffer_char, termina cuando no es un caracter ni underscore
+				buffer_char(c, i);
+				i++;
+			}
+			ungetc(c,archive);
+
+			return check_reserved(); //Revisa si el string encontrado es una palabra reservada
+
+		}  else if (isdigit(in_char)) {
+			buffer_char(in_char, i);
+			i++;
+			for (c = getc(archive); isdigit(c); c = getc(archive)){
+				buffer_char(c, i);
+				i++;
+			}
+			ungetc(c,archive);
+
+			//token_buffer[ncol] = '\0';
+			//printf( "Token_Buffer = %s\n", &token_buffer[0]);
+
+			return INTLITERAL;
+
+		} else if (in_char == '(')
+			return LPAREN;
+		else if (in_char == ')')
+			return RPAREN;
+		else if (in_char == ';')
+			return SEMICOLON;
+		else if (in_char == ',')
+			return COMMA;
+		else if (in_char == '+') {
+			printf("caracter actual in_char es:%c\n",in_char);
+			printf("caracter actual c es:%c\n",c);
+			buffer_char(in_char, i);
+			i++;
+			return PLUSOP;
+		}
+		else if (in_char == ':') {
+			/* looking for ":=" */
+			c = getc(archive);
+			if (c == '=') {
+				return ASSIGNOP;
+			}
+			else {
+				ungetc(c,archive);
+				lexical_error();//
+			}
+		} else if (in_char == '-') {
+			printf("entro al comentario/menos \n");
+			/* is it --, comment start */
+			c = getc(archive);
+			if (c == '-') {
+				printf("entro al comentario \n");
+				do
+				{
+					in_char = getc(archive);
+				} while (in_char != '\n');
+			} else {
+				printf("caracter actual in_char es:%c\n",in_char);
+				printf("caracter actual c es:%c\n",c);
+				buffer_char(in_char, i);
+				i++;
+				ungetc(c,archive);
+				return MINUSOP;
+			}
+		}  else
+			lexical_error();
+	}
+
+	return 0;
+}
+
+
+
+// int esEstadoFinal(int e){
+// 	switch (e) {
+// 	case 0: case 1: case 3: case 11: case 14:
+// 		return 0;
 // 	}
-//
-// 	return 0;
+// 	return 1;
 // }
 
 
 
-int esEstadoFinal(int e){
-	switch (e) {
-	case 0: case 1: case 3: case 11: case 14:
-		return 0;
-	}
-	return 1;
-}
+// token scanner(void){
+// 	char c;
+// 	int col, i = 0, estado_actual = 0;
 
+// 	static int tabla [15][13]={
+// 		{1,3,5,6,7,8,9,10,11,14,13,0,14},
+// 		{1,1,2,2,2,2,2,2,2,2,2,2,2},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{4,3,4,4,4,4,4,4,4,4,4,4,4},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,12,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,12,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14},
+// 		{14,14,14,14,14,14,14,14,14,14,14,14,14}
+// 	};
 
+// 	clear_buffer();
 
-token scanner(void){
-	char c;
-	int col, i = 0, estado_actual = 0;
+// 	do {
+// 		c = getc(archive);
+// 		col = columna(c);
+// 		estado_actual = tabla[estado_actual][col];
+// 		if (col!= 11) {
+// 			buffer_char(c, i);
+// 			i++;
+// 		}
+// 	} while (!esEstadoFinal(estado_actual));
 
-	static int tabla [15][13]={
-		{1,3,5,6,7,8,9,10,11,14,13,0,14},
-		{1,1,2,2,2,2,2,2,2,2,2,2,2},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{4,3,4,4,4,4,4,4,4,4,4,4,4},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,12,14,14,14},
-		{14,14,14,14,14,14,14,14,14,12,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14},
-		{14,14,14,14,14,14,14,14,14,14,14,14,14}
-	};
+// 	switch(estado_actual) {
+// 	case 2:
+// 		if (col != 11) {
+// 			ungetc(c, archive);
+// 			token_buffer[i-1] = '\0';
+// 		}
+// 		return ID;
+// 	case 4:
+// 		if (col != 11) {
+// 			ungetc(c, archive);
+// 			token_buffer[i-1] = '\0';
+// 		}
+// 		return INTLITERAL;
+// 	case 5:
+// 		return PLUSOP;
+// 	case 6:
+// 		return MINUSOP;
+// 	case 7:
+// 		return LPAREN;
+// 	case 8:
+// 		return RPAREN;
+// 	case 9:
+// 		return COMMA;
+// 	case 10:
+// 		return SEMICOLON;
+// 	case 12:
+// 		return ASSIGNOP;
+// 	case 13:
+// 		return SCANEOF;
+// 	case 14:
+// 		return LEXICALERROR;
 
-	clear_buffer();
+// 	}
+// 	return SCANEOF;
+// }
 
-	do {
-		c = getc(archive);
-		col = columna(c);
-		estado_actual = tabla[estado_actual][col];
-		if (col!= 11) {
-			buffer_char(c, i);
-			i++;
-		}
-	} while (!esEstadoFinal(estado_actual));
+// int columna(int c){
+// 	if(isalpha(c)) {
+// 		return 0;
+// 	}else if (isdigit(c)) {
+// 		return 1;
+// 	}else if (isspace(c)) {
+// 		return 11;
+// 	}
+// 	switch (c) {
+// 	case '+':
+// 		return 2;
+// 	case '-':
+// 		return 3;
+// 	case '(':
+// 		return 4;
+// 	case ')':
+// 		return 5;
+// 	case ',':
+// 		return 6;
+// 	case ';':
+// 		return 7;
+// 	case ':':
+// 		return 8;
+// 	case '=':
+// 		return 9;
+// 	case EOF:
+// 		return 10;
+// 	default:
+// 		return 12;
 
-	switch(estado_actual) {
-	case 2:
-		if (col != 11) {
-			ungetc(c, archive);
-			token_buffer[i-1] = '\0';
-		}
-		return ID;
-	case 4:
-		if (col != 11) {
-			ungetc(c, archive);
-			token_buffer[i-1] = '\0';
-		}
-		return INTLITERAL;
-	case 5:
-		return PLUSOP;
-	case 6:
-		return MINUSOP;
-	case 7:
-		return LPAREN;
-	case 8:
-		return RPAREN;
-	case 9:
-		return COMMA;
-	case 10:
-		return SEMICOLON;
-	case 12:
-		return ASSIGNOP;
-	case 13:
-		return SCANEOF;
-	case 14:
-		return LEXICALERROR;
-
-	}
-	return SCANEOF;
-}
-
-int columna(int c){
-	if(isalpha(c)) {
-		return 0;
-	}else if (isdigit(c)) {
-		return 1;
-	}else if (isspace(c)) {
-		return 11;
-	}
-	switch (c) {
-	case '+':
-		return 2;
-	case '-':
-		return 3;
-	case '(':
-		return 4;
-	case ')':
-		return 5;
-	case ',':
-		return 6;
-	case ';':
-		return 7;
-	case ':':
-		return 8;
-	case '=':
-		return 9;
-	case EOF:
-		return 10;
-	default:
-		return 12;
-
-	}
-}
+// 	}
+// }
 
 //Esta función tiene algo malo, no me pregunten qué, pero funciona mejor la de la progra en esp.
 // /* Agrega su argumento a un buffer de caracteres llamado token_buffer - AgregarCaracter()*/
@@ -448,11 +465,18 @@ int columna(int c){
 /* Adds argument to token_buffer*/
 void buffer_char(int c, int posicion){
 	token_buffer[posicion] = c;
+	printf("\nSe imprime %c\n",c);
 }
 
 // Reset the token string to be empty
 void clear_buffer(void){
+	printf("Se limpio el buffer\n");
 	memset(token_buffer,0,strlen(token_buffer));
+	int i;
+	for (i=0; i < strlen(token_buffer); i++){
+		token_buffer[i]='\0';
+	}
+	
 }
 
 /*-----------------------------Semantic routines-------------------------------*/
@@ -477,6 +501,7 @@ expr_rec process_id(void){
 
 /* Produce operator descriptor. (Diff on book)*/
 char * process_op(void){
+	printf("El operador es %s \n",token_buffer);
 	return token_buffer;
 }
 
@@ -534,10 +559,14 @@ expr_rec gen_infix(expr_rec e1, char * op, expr_rec e2){
 	char cadenaTemporal[MAXIDLEN] = "Temp&";
 	char numero[MAXIDLEN];
 	char cadenaOperador[MAXIDLEN];
-
+	printf("Genera codigo aaaaa%c\n",op[0]);
+	printf("Genera codigo aaaaa%s\n",op);
+	printf("el buffer tiene: %s\n",token_buffer);
 	if (op[0] == '-')
+	printf("Genera codigo para sub \n");
 		strcpy(cadenaOperador, "Sub");
 	if (op[0] == '+')
+	printf("Genera codigo para add \n");
 		strcpy(cadenaOperador, "Add");
 	sprintf(numero, "%d", numeroVariableTemporal);
 	numeroVariableTemporal++;
@@ -594,7 +623,7 @@ int lookup(char * id, RegTS * TS, token * t){
 
 /*Enters a symbol at the last spot of the symbol table (before $)*/
 void enter(char * id, RegTS * TS){
-	int i = 4;
+	int i = 0;
 	while (strcmp("$", TS[i].identifier)) i++;
 	if (i <= 999) {
 		strcpy(TS[i].identifier, id );
