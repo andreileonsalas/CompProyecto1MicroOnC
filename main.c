fb9e3b741a4d9ffd6ef930ce8c29a199e1ca7155
Andrei Leon Salas <andreileonsalas@gmail.com>
2020-10-09 02:34:10 -0600

    update main

--- main.c
+++ main.c
@@ -64,9 +64,9 @@ int main(int argc, char* argv[]) {
 
 	// printf("Compilando el codigo\n");
 	// system("nasm -f elf64 microasm.asm");
-	// system(" gcc microasm.o -o micro");
+	// system(" gcc -fPIC microasm.o -o microasm");
 	// printf("Ejecutando el codigo\n");
-	// system("./micro");
+	// system("./microasm");
 	
 
 	return EXIT_SUCCESS;
